fb35870193288f51e414ceed71916d2a60e96404
vaishnav-197 <vaishnavea18@gmail.com>
2021-12-05 03:38:52 +0530

    layout added

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+
+
+
+const routes = require('./routes/routes')
+
+const app = express()
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public'));
+
+
+app.set('view engine', 'ejs');
+
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(
+    morgan(
+     ':method :url :status :response-time ms'
+    ))
+app.use('/',routes);
+
+
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+    console.log('listening on port '+PORT);
+    console.log('http://localhost:4000/');
+   });
